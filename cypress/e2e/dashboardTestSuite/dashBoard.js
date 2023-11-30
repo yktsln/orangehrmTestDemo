5288b4699e46cd8e524b95e765a5898ad6fbcde6
Yekta Aslan <yktsln@users.noreply.github.com>
2023-11-30 17:45:22 +0100

    dashBoard test suite completed

--- cypress/e2e/dashboardTestSuite/dashBoard.js
+++ cypress/e2e/dashboardTestSuite/dashBoard.js
@@ -20,7 +20,7 @@ export class dashBoardTest{
         cy.get('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.orangehrm-dashboard-widget').each(($list,index)=>{
             if(index === 0){
                 cy.wrap($list).should('exist');
-                cy.wrap($list).find('.oxd-text.oxd-text--p').contains('Time at Work');
+                cy.wrap($list).find('.oxd-text.oxd-text--p').contains('Time at Work').should('be.visible');
                 cy.wrap($list).find('.oxd-icon.bi-clock-fill.orangehrm-dashboard-widget-icon').should('be.visible');
                 cy.wrap($list).find('.orangehrm-attendance-card-profile-image img').should('have.attr','src','../pim/viewPhoto/empNumber/7');
                 cy.wrap($list).find('.orangehrm-attendance-card-profile-record p').should('be.visible');
@@ -41,12 +41,112 @@ export class dashBoardTest{
                 cy.wrap($list).find('.emp-attendance-chart canvas').should('have.attr', 'height', '170');
 
                 cy.log('Dashboard widget test completed for Time at Work');
-
-                
-
             }
         })
     }
+
+    
+    myActions(){
+         cy.get('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.orangehrm-dashboard-widget').each(($list,index)=>{
+            if(index === 1){
+                cy.wrap($list).should('exist').should('be.visible');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-name p').contains('My Actions');
+                cy.wrap($list).find('.orangehrm-todo-list').each(($elements,secondIndex)=>{
+                    if(secondIndex === 0){
+                       cy.wrap($elements).eq(0).should('exist').should('be.visible');
+                       cy.wrap($elements).find('.orangehrm-report-icon').eq(0).should('be.enabled');
+                       cy.wrap($elements).find('.orangehrm-report-icon svg').eq(0).should('have.attr','xmlns','http://www.w3.org/2000/svg');
+                       cy.wrap($elements).find('.orangehrm-todo-list-item p').eq(0).should('have.text',' (3) Candidates to Interview'); 
+                    }
+                })
+
+            }
+         })
+        
+    }
+
+    quickLaunch(){
+         cy.get('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.orangehrm-dashboard-widget').each(($list,index)=>{
+            if(index === 2){
+                cy.wrap($list).should('exist').should('be.visible');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-name p').contains('Quick Launch');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-name i').should('exist').should('be.visible');
+                cy.wrap($list).find('.orangehrm-quick-launch div[class]').should('exist').should('be.visible');
+                cy.wrap($list).find('.orangehrm-quick-launch button').should('exist').should('be.visible').should('be.enabled');
+                
+            }
+         });
+    }
+
+    blatestPost(){
+        cy.get('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.orangehrm-dashboard-widget').each(($list,index)=>{
+            if(index === 3){
+                cy.wrap($list).should('exist').should('be.visible');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-name p').should('exist').should('be.visible').contains('Buzz Latest Posts');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-name p').should('exist').should('be.visible');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-body div[class]').should('exist').should('be.visible')
+                .each(($elements,index)=>{
+                    if(index === 0){
+                        cy.wrap($elements).find('.orangehrm-buzz-widget-header p').should('be.visible');
+                        cy.wrap($elements).find('.orangehrm-buzz-widget-card img').should('be.visible');
+                    }//in ths widget posts changes everytime run the test
+                });
+            }
+        });
+    }
+
+    employeesonLeaveToday(){
+        cy.get('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.orangehrm-dashboard-widget').each(($list,index)=>{
+            if(index === 4){
+                
+                cy.wrap($list).find('.orangehrm-dashboard-widget-name').should('have.text','Employees on Leave Today').should('be.visible').should('exist');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-name svg').should('have.attr','xmlns','http://www.w3.org/2000/svg');
+                cy.wrap($list).find('.orangehrm-leave-card-icon').should('exist').should('be.visible');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-body img').should('have.attr','src','/web/images/dashboard_empty_widget_watermark.png').should('be.visible').should('exist');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-body p').should('have.text','No Employees are on Leave Today').should('be.visible').should('exist');
+            }
+        })
+    }
+
+    distributionBySubUnit(){
+        cy.get('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.orangehrm-dashboard-widget').each(($list,index)=>{
+            if(index === 5){
+                cy.wrap($list).find('.orangehrm-dashboard-widget-header p').should('have.text','Employee Distribution by Sub Unit').should('exist').should('be.visible');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-header i').should('exist').should('be.visible');
+                cy.wrap($list).find('.emp-distrib-chart .oxd-chart-legend').should('exist').then(()=>{
+                    const departments = ['Engineering', 'Sales & Marketing', 'Administration', 'Finance', 'Human Resources', 'Client Services', 'Unassigned'];
+                    departments.forEach((department) => {
+                    cy.get('.emp-distrib-chart .oxd-chart-legend').contains(department).should('exist');
+                     });
+                })
+            }
+        })
+
+    }
+
+    EmployeeDistributionbyLocation(){
+        cy.get('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.orangehrm-dashboard-widget').each(($list,index)=>{
+            if(index === 6){
+                cy.wrap($list).find('.orangehrm-dashboard-widget-header p').should('have.text','Employee Distribution by Location').should('exist');
+                cy.wrap($list).find('.orangehrm-dashboard-widget-header i').should('exist');
+                cy.wrap($list).find('.oxd-chart-legend').should('exist').then(()=>{
+                    const locations = ['New York Sales Office','Canadian Regional HQ','Texas R&D','HQ - CA, USA','Unassigned'];
+                    locations.forEach((location)=>{
+                        cy.get('.oxd-chart-legend').contains(location).should('exist');
+                    }) 
+                })
+            }
+        })
+    }
+
+    footer(){
+        cy.get('.oxd-layout-footer p').eq(0).should('have.text','OrangeHRM OS 5.5').should('exist');
+        cy.get('.oxd-layout-footer p').eq(1).should('have.text','© 2005 - 2023 OrangeHRM, Inc. All rights reserved.').should('exist');
+        cy.get('.oxd-layout-footer a').should('have.attr','href','http://www.orangehrm.com').should('exist');
+
+    }
+
+
 
 
 }
--- cypress/e2e/dashboardTestSuite/dashBoardTestSuite.cy.js
+++ cypress/e2e/dashboardTestSuite/dashBoardTestSuite.cy.js
@@ -11,7 +11,38 @@ describe('dashboard Test Suite',()=>{
 
     it('all dashboard widgets',()=>{
         dashBoard.dashBoards();
+    })
+
+    it('Time at Work widget test cases',()=>{
         dashBoard.testAtWork();
+    })
+
+    it('My actions widget test cases',()=>{
+        dashBoard.myActions();
+    })
+
+    it('Quick launch widget test cases',()=>{
+        dashBoard.quickLaunch();
+    })
+
+    it('Buzz Latest Posts widget test cases',()=>{
+        dashBoard.blatestPost();
+    })
+
+    it('Employees on Leave Today widget test cases',()=>{
+        dashBoard.employeesonLeaveToday();
+    })
+
+    it('Distribution by Sub Unit widget test cases',()=>{
+     dashBoard.distributionBySubUnit();
+    })
+
+    it('Employee Distribution by Location widget test cases',()=>{
+        dashBoard.EmployeeDistributionbyLocation();
+    })
+
+    it.only('footer test cases',()=>{
+        dashBoard.footer();
     })
 
 })
