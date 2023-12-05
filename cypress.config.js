2771c74cce1f1aef5e339a06e7dee309c314273d
Yekta Aslan <yktsln@users.noreply.github.com>
2023-12-05 12:32:28 +0100

    new

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-     projectId: "nmouq1",
+    projectId: "nmouq1",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-   
+  },
 
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
   },
 });
